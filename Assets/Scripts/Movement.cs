63d1b0c89ae8a0aecedee087d2be2f67c50c09cc
Mehmet Baturalp ÇAYLAK <92629919+Tringoala@users.noreply.github.com>
2024-04-13 09:44:55 +0300

    13.04.2024

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,19 +1,51 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    InputActionReference reference;
+
+    CinemachineVirtualCamera cm;
+
+
     Rigidbody rb;
+    [SerializeField]
+    float moveSpeed;
+    private Vector3 _movedirection;
+    public GameObject head;
     bool isMove;
+
+    float locked;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         rb = gameObject.GetComponent<Rigidbody>();
+        locked = 5f;
     }
-
+    private void Update()
+    {
+        if (locked <= 0)
+        {
+            gameObject.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+        }
+        else
+        {
+            locked -= Time.deltaTime;
+        }
+    }
     private void FixedUpdate()
     {
+        _movedirection = reference.action.ReadValue<Vector3>();
+        Debug.Log(_movedirection);
+        rb.AddForce(moveSpeed * Time.fixedDeltaTime * _movedirection);
+//        if()
     }
 
     public void OnPressedClassicMovement(InputValue ýnputValue)
